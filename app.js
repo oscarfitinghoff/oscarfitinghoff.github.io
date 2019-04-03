5ecf6d266c6245d87890d24b1d1299baedfe687c
Oscar <oscar.fitinghoff@gmail.com>
2019-04-03 13:13:21 +0200

    Added checkout1 and checkout2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var btnAddToCart = document.querySelector('.js-addToCart');
-var btnCheckout = document.querySelector('.js-checkout');
+var btnCheckout1 = document.querySelector('.js-checkout1');
+var btnCheckout2 = document.querySelector('.js-checkout2');
 var btnPurchase = document.querySelector('.js-purchase');
+
 btnAddToCart.addEventListener('click', function(e) {
   e.preventDefault();
   console.log('btnAddToCart');
@@ -24,14 +26,42 @@ btnAddToCart.addEventListener('click', function(e) {
   });
 });
 
-btnCheckout.addEventListener('click', function(e) {
+btnCheckout1.addEventListener('click', function(e) {
   e.preventDefault();
-  console.log('btnCheckout');
+  console.log('btnCheckout1');
   dataLayer.push({
     event: 'checkout',
     ecommerce: {
       checkout: {
         actionField: { step: '1', option: 'Osäker_1' },
+        products: [
+          {
+            name: 'Gåva 1',
+            id: '12345',
+            price: '200',
+            brand: 'Privat',
+            category: 'TestGåvor',
+            quantity: 1
+          }
+        ]
+      }
+    },
+    eventCallback: function() {
+      console.log('checkout callback');
+      // document.location = 'checkout.html';
+      // if (cb) cb();
+    }
+  });
+});
+
+btnCheckout2.addEventListener('click', function(e) {
+  e.preventDefault();
+  console.log('btnCheckout2');
+  dataLayer.push({
+    event: 'checkout',
+    ecommerce: {
+      checkout: {
+        actionField: { step: '2', option: 'Osäker_1' },
         products: [
           {
             name: 'Gåva 1',
@@ -59,7 +89,7 @@ btnPurchase.addEventListener('click', function(e) {
     ecommerce: {
       purchase: {
         actionField: {
-          id: 'T12345',
+          id: 'T123456',
           affiliation: 'SRK',
           revenue: '35.43',
           tax: '0',
